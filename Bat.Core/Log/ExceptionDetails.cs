8ecc3ee593db33738e63725e1f6073c3a17594d3
Mehran <mehrannoruzi@gmail.com>
2020-10-28 11:51:07 +0330

    Add File To Source Control

---
+++ Bat.Core/Log/ExceptionDetails.cs
@@ -0,0 +1,9 @@
+﻿namespace Bat.Core
+{
+    public class ExceptionDetails
+    {
+        public string MethodName { get; set; }
+        public string Parameters { get; set; }
+        public int ExceptionLineNumber { get; set; }
+    }
+}
