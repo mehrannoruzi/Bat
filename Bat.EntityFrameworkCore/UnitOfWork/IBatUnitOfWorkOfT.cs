dabf011e9b2651424f5a88d2d1cacfa4a1e463c9
Mehran Norouzi <mehrannoruzi@gmail.com>
2021-01-30 12:20:29 +0330

    Rename UnitOfWorks

--- Bat.EntityFrameworkCore/UnitOfWork/IBatUnitOfWorkOfT.cs
+++ Bat.EntityFrameworkCore/UnitOfWork/IBatUnitOfWorkOfT.cs
@@ -1,9 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
 namespace Bat.EntityFrameworkCore
 {
-    public interface IBatUnitOfWorkOfT<TContext> : IBatUnitOfWork where TContext : DbContext
+    public interface IBatUnitOfWorkOfT<TContext> : IBatUnitOfWork, IDisposable where TContext : BatDbContext
     {
         Task<int> SaveChangesAsync(bool ensureAutoHistory = false, params IBatUnitOfWork[] unitOfWorks);
     }
