e749fcb40d465901a9c27f8705ec3de511aa210a
Mehran Norouzi <mehrannoruzi@gmail.com>
2021-02-06 19:58:58 +0330

    Add MenuModel & MenuExtension

---
+++ Bat.Core/EntityModel/MenuModel.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Bat.Core
+{
+    public class MenuModel
+    {
+        public int MenuId { get; set; }
+        public int? ParentId { get; set; }
+        public bool ShowInMenu { get; set; }
+        public byte OrderPriority { get; set; }
+        public string Path { get; set; }
+        public string Name { get; set; }
+        public string Icon { get; set; }
+        public bool IsDefault { get; set; }
+        public int RoleId { get; set; }
+        public string RoleName { get; set; }
+        [JsonIgnore]
+        public string Menus { get; set; }
+
+
+        [NotMapped]
+        public bool HasChild { get { return !string.IsNullOrWhiteSpace(Menus); } }
+
+        [NotMapped]
+        public bool HavePath { get { return !string.IsNullOrWhiteSpace(Path); } }
+
+        [NotMapped]
+        public List<MenuModel> ChildMenus { get { return JsonConvert.DeserializeObject<List<MenuModel>>(Menus ?? "[]"); } }
+    }
+}
---
+++ Bat.Core/Extensions/UserMenuExtension.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Bat.Core
+{
+    public static class UserMenuExtension
+    {
+        public static IEnumerable<MenuModel> GetAllMenu(this IEnumerable<MenuModel> userMenus)
+        {
+            if (userMenus.IsNull()) return null;
+
+            var result = new List<MenuModel>();
+            foreach (var menu in userMenus)
+            {
+                result.Add(menu);
+                if (menu.HasChild) result.AddRange(menu.ChildMenus);
+            }
+
+            return result;
+        }
+    }
+}
