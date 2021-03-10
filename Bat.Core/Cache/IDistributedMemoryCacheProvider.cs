13dc1d648cc2d438142bb692e1d3b4871f0fab2a
Mehran Norouzi <mehrannoruzi@gmail.com>
2021-03-10 21:32:52 +0330

    Add Cache Interface

---
+++ Bat.Core/Cache/IDistributedMemoryCacheProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Bat.Core
+{
+    public interface IDistributedMemoryCacheProvider : ISingletonInjection
+    {
+        public Task<bool> SetAsync(string key, string value, TimeSpan? expiry = null);
+        public Task<string> GetAsync(string key);
+        public Task<bool> DeleteAsync(string key);
+        public Task RenameAsync(string oldKey, string newKey);
+        public Task<bool> ExistsAsync(string Key);
+        public Task<bool> ExpireAsync(string Key, TimeSpan? expiry = null);
+        public Task<bool> PersistAsync(string Key);
+        public Task<TimeSpan?> IdleAsync(string Key);
+        public Task<long> IncrementAsync(string Key);
+        public Task<long> DecrementAsync(string Key);
+    }
+}
---
+++ Bat.Core/Cache/IMemoryCacheProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bat.Core
+{
+    public interface IMemoryCacheProvider : ISingletonInjection
+    {
+        bool Add(string key, object value, DateTimeOffset expireTime);
+        //bool Add(string key, object value, CacheItemPolicy cachePolicy);
+        bool Add(string key, object value, DateTimeOffset expireTime, TimeSpan slidingTime);
+        object AddOrGetExisting(string key, object value, DateTimeOffset expireTime);
+        //object AddOrGetExisting(string key, object value, CacheItemPolicy cachePolicy);
+        bool Remove(string key);
+        object Get(string key);
+        //CacheItem GetCacheItem(string key);
+        IDictionary<string, object> GetAllItem(IEnumerable<string> keys);
+        long GetCount();
+        long GetSize();
+        long GetMemoryLimit();
+        object TrimMemory(int percentage);
+    }
+}
