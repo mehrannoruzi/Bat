8f1bb4dcc9ac8383affd2bc932cef158d474e419
Mehran Norouzi <mehrannoruzi@gmail.com>
2021-03-26 16:48:15 +0430

    Fix ModelValidationFilter

--- Bat.AspNetCore/Filter/BatModelValidationFilter.cs
+++ Bat.AspNetCore/Filter/BatModelValidationFilter.cs
@@ -9,28 +9,25 @@ namespace Bat.AspNetCore
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
     public class BatModelValidationAttribute : ActionFilterAttribute
     {
-        public override Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        public async override Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
         {
             if (!context.ModelState.IsValid)
             {
                 context.Result = new BadRequestObjectResult(new Response<object> { Message = context.ModelState.GetModelError() });
-                return Task.CompletedTask;
+                await Task.CompletedTask;
             }
             else
-                return base.OnActionExecutionAsync(context, next);
+                await base.OnActionExecutionAsync(context, next);
         }
 
-        //public override void OnActionExecuting(ActionExecutingContext context)
-        //{
-        //    if (!context.ModelState.IsValid)
-        //    {
-        //        //var errorMessage = string.Empty;
-        //        //foreach (var error in context.ModelState)
-        //        //    errorMessage += $"Key:{error.Key} Value:{error.Value} | ";
-        //        //FileLoger.Message(context.ModelState.GetModelError());
-
-        //        context.Result = new BadRequestObjectResult(context.ModelState);
-        //    }
-        //}
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                context.Result = new BadRequestObjectResult(new Response<object> { Message = context.ModelState.GetModelError() });
+            }
+            else
+                base.OnActionExecuting(context);
+        }
     }
 }
