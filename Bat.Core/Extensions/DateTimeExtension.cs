0e39ecb058a8a2c884d74ca72e43eafe62d14ecf
Mehran Norouzi <mehrannoruzi@gmail.com>
2021-07-12 15:59:49 +0430

    Extend DateTime ExtensionMethod

--- Bat.Core/Extensions/DateTimeExtension.cs
+++ Bat.Core/Extensions/DateTimeExtension.cs
@@ -4,8 +4,12 @@ namespace Bat.Core
 {
     public static class DateTimeExtension
     {
+        public static DateTime ToDateTime(this string persianDateTime) => PersianDateTime.Parse(persianDateTime).ToDateTime();
+
+        public static DateTime ToDateTime(this string persianDateTime, int hour, int minute) => PersianDateTime.Parse(persianDateTime).ToDateTime().AddHours(hour).AddMinutes(minute);
+
         public static PersianDateTime ToPersianDateTime(this DateTime date) => PersianDateTime.Parse(date);
-        
+
         public static string ToPersianDate(this DateTime date) => PersianDateTime.Parse(date).ToString(PersianDateTimeFormat.Date);
 
         public static bool IsFuture(this DateTime date) => date.IsFuture(DateTime.Now);
