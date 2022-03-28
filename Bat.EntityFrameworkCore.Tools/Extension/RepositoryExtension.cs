f1624ad7531ee3dd058371ef6c264bf970dcac57
Mehran Norouzi <mehrannoruzi@gmail.com>
2022-03-28 21:21:20 +0430

    Some Refactore

---
+++ Bat.EntityFrameworkCore.Tools/Extension/RepositoryExtension.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Bat.EntityFrameworkCore.Tools;
+
+public static class RepositoryExtension
+{
+    public static IEFGenericRepo<T> GetRepository<T>(this IServiceProvider serviceProvider) where T : class, IBaseEntity
+    {
+        return serviceProvider.GetService<IEFGenericRepo<T>>();
+    }
+}
