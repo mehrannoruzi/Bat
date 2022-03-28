982d9d9419a7209e92e623020970dffa80058992
Mehran Norouzi <mehrannoruzi@gmail.com>
2022-03-28 21:20:58 +0430

    Add IDapperGenericRepo

---
+++ Bat.Dapper/Repositories/IDapperGenericRepo.cs
@@ -0,0 +1,12 @@
+﻿namespace Bat.Dapper;
+
+public interface IDapperGenericRepo<T>
+{
+    Task<int> AddAsync(string sqlStatement, object parameters = null, IDbTransaction transaction = null, int? commandTimOut = null);
+    Task<bool> UpdateAsync(string sqlStatement, object parameters = null, IDbTransaction transaction = null, int? commandTimOut = null);
+    Task<bool> DeleteAsync(string sqlStatement, object parameters = null, IDbTransaction transaction = null, int? commandTimOut = null);
+    Task<T> FirstOrDefaultAsync(string sqlStatement, object parameters = null, IDbTransaction transaction = null, int? commandTimOut = null);
+    Task<bool> AnyAsync(string sqlStatement, object parameters = null, IDbTransaction transaction = null, int? commandTimOut = null);
+    List<T> Get(string sqlStatement, object parameters = null, IDbTransaction transaction = null, bool buffered = true, int? commandTimOut = null);
+    PagingListDetails<T> GetPaging(string sqlStatement, PagingParameter pagingParameter, object parameters = null, IDbTransaction transaction = null, bool buffered = false, int? commandTimOut = null);
+}
