edd1cc7f8c9320419749855b968bb6c2bf95ee5d
Mehran Norouzi <mehrannoruzi@gmail.com>
2022-10-12 15:27:08 +0330

    Fix Encryption Algorithms

--- Bat.Core/Security/AesAlgorithm.cs
+++ Bat.Core/Security/AesAlgorithm.cs
@@ -9,19 +9,22 @@ public class AesAlgorithm
     {
         var initVectorBytes = Encoding.ASCII.GetBytes(initVector);
         var saltValueBytes = Encoding.ASCII.GetBytes(saltValue);
-        var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
         var password = new PasswordDeriveBytes(passPhrase, saltValueBytes, hashAlgorithm, passwordIterations);
         var keyBytes = password.GetBytes(keySize / 8);
         var symmetricKey = Aes.Create();
-        var encryptor = symmetricKey.CreateEncryptor(keyBytes, initVectorBytes);
-        var memoryStream = new MemoryStream();
-        var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write);
-        cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
-        cryptoStream.FlushFinalBlock();
-        byte[] cipherTextBytes = memoryStream.ToArray();
-        memoryStream.Close();
-        cryptoStream.Close();
-
+        symmetricKey.Key = keyBytes;
+        symmetricKey.IV = initVectorBytes;
+        var encryptor = symmetricKey.CreateEncryptor();
+        byte[] cipherTextBytes;
+        using (var memoryStream = new MemoryStream())
+        {
+            using var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write);
+            using (var streamWriter = new StreamWriter(cryptoStream))
+            {
+                streamWriter.Write(plainText);
+            }
+            cipherTextBytes = memoryStream.ToArray();
+        }
         return Convert.ToBase64String(cipherTextBytes);
     }
 
@@ -33,15 +36,13 @@ public class AesAlgorithm
         var password = new PasswordDeriveBytes(passPhrase, saltValueBytes, hashAlgorithm, passwordIterations);
         var keyBytes = password.GetBytes(keySize / 8);
         var symmetricKey = Aes.Create();
-        var decryptor = symmetricKey.CreateDecryptor(keyBytes, initVectorBytes);
-        var memoryStream = new MemoryStream(cipherTextBytes);
-        var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read);
-        var plainTextBytes = new byte[cipherTextBytes.Length];
-        var decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
-        memoryStream.Close();
-        cryptoStream.Close();
-        string plainText = Encoding.UTF8.GetString(plainTextBytes, 0, plainTextBytes.Length);
-
-        return plainText;
+        symmetricKey.Key = keyBytes;
+        symmetricKey.IV = initVectorBytes;
+        var decryptor = symmetricKey.CreateDecryptor();
+        using var memoryStream = new MemoryStream(cipherTextBytes);
+        using var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read);
+        using var streamReader = new StreamReader(cryptoStream);
+        return streamReader.ReadToEnd();
     }
+
 }
--- Bat.Core/Security/RijndaelAlgorithm.cs
+++ Bat.Core/Security/RijndaelAlgorithm.cs
@@ -12,16 +12,23 @@ public class RijndaelAlgorithm
         var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
         var password = new PasswordDeriveBytes(passPhrase, saltValueBytes, hashAlgorithm, passwordIterations);
         var keyBytes = password.GetBytes(keySize / 8);
-        var symmetricKey = new RijndaelManaged { Mode = CipherMode.CBC };
-        var encryptor = symmetricKey.CreateEncryptor(keyBytes, initVectorBytes);
-        var memoryStream = new MemoryStream();
-        var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write);
-        cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
-        cryptoStream.FlushFinalBlock();
-        byte[] cipherTextBytes = memoryStream.ToArray();
-        memoryStream.Close();
-        cryptoStream.Close();
-
+        var symmetricKey = new RijndaelManaged
+        {
+            Mode = CipherMode.CBC,
+            Key = keyBytes,
+            IV = initVectorBytes
+        };
+        var encryptor = symmetricKey.CreateEncryptor();
+        byte[] cipherTextBytes;
+        using (var memoryStream = new MemoryStream())
+        {
+            using var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write);
+            using (var streamWriter = new StreamWriter(cryptoStream))
+            {
+                streamWriter.Write(plainText);
+            }
+            cipherTextBytes = memoryStream.ToArray();
+        }
         return Convert.ToBase64String(cipherTextBytes);
     }
 
@@ -32,16 +39,16 @@ public class RijndaelAlgorithm
         var cipherTextBytes = Convert.FromBase64String(cipherText);
         var password = new PasswordDeriveBytes(passPhrase, saltValueBytes, hashAlgorithm, passwordIterations);
         var keyBytes = password.GetBytes(keySize / 8);
-        var symmetricKey = new RijndaelManaged { Mode = CipherMode.CBC };
-        var decryptor = symmetricKey.CreateDecryptor(keyBytes, initVectorBytes);
-        var memoryStream = new MemoryStream(cipherTextBytes);
-        var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read);
-        var plainTextBytes = new byte[cipherTextBytes.Length];
-        var decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
-        memoryStream.Close();
-        cryptoStream.Close();
-        string plainText = Encoding.UTF8.GetString(plainTextBytes, 0, decryptedByteCount);
-
-        return plainText;
+        var symmetricKey = new RijndaelManaged
+        {
+            Mode = CipherMode.CBC,
+            Key = keyBytes,
+            IV = initVectorBytes
+        };
+        var decryptor = symmetricKey.CreateDecryptor();
+        using var memoryStream = new MemoryStream(cipherTextBytes);
+        using var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read);
+        using var streamReader = new StreamReader(cryptoStream);
+        return streamReader.ReadToEnd();
     }
 }
