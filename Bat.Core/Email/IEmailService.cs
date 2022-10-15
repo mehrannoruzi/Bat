46a4a9a6edb3e7addd30394146fb34082bbc73f8
Mehran Norouzi <mehrannoruzi@gmail.com>
2022-10-15 21:09:48 +0330

    Update To Net7 rc2

--- Bat.Core/Email/IEmailService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Bat.Core;
-
-public interface IEmailService
-{
-    IResponse<List<bool>> Send(string from, List<string> to, EmailMessage email);
-    IResponse<List<bool>> Send(List<string> from, List<string> to, EmailMessage email);
-    IResponse<List<bool>> Send(List<string> from, List<string> to, List<EmailMessage> email);
-}
