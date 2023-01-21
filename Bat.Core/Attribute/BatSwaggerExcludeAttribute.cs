a25e5aec33ab7df302a2be0e204ecd5ea3635926
Mehran Norouzi <mehrannoruzi@gmail.com>
2023-01-21 10:09:27 +0330

    Refactor

--- Bat.Core/Attribute/BatSwaggerExcludeAttribute.cs
+++ Bat.Core/Attribute/BatSwaggerExcludeAttribute.cs
@@ -1,4 +1,4 @@
-﻿namespace Bat.Tools;
+﻿namespace Bat.Core;
 
 [AttributeUsage(AttributeTargets.Property | AttributeTargets.Class | AttributeTargets.Field, AllowMultiple = true)]
 public class SwaggerExcludeAttribute : Attribute
