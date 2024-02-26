ba29b1bd9a6911bb14d726d1814379519740b97b
Mehran Norouzi <mehrannoruzi@gmail.com>
2024-02-26 21:34:27 +0330

    Extend Serialization Extensions

--- Bat.Core/GlobalUsing.cs
+++ Bat.Core/GlobalUsing.cs
@@ -1,5 +1,6 @@
 ﻿global using System;
 global using System.Linq;
+global using System.Text.Json;
 global using System.ComponentModel;
 global using System.Collections.Generic;
 global using System.Text.Json.Serialization;
